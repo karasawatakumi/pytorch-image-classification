ba837f51910eee4e5f1d85081ef2ec4b509914a0
Takumi Karasawa <zaki19930927@gmail.com>
2022-04-12 23:06:47 +0900

    modify importing lines

--- train.py
+++ train.py
@@ -7,8 +7,7 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
-from pytorch_lightning import LightningModule, LightningDataModule
-from pytorch_lightning import Trainer
+from pytorch_lightning import LightningModule, LightningDataModule, Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.utilities.seed import seed_everything
 from torch.utils.data import DataLoader
